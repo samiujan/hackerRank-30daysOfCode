0b1e2b3b64a003e4c8b33e9f5badbaecddd574b1
Sami Jan <sami.jan@gmail.com>
2016-01-10 03:18:51 +0500

    add day6

--- day6.py
+++ day6.py
@@ -1,11 +1,4 @@
-T = input()
-l = []
-for _ in range(T):
-  l.append(map(int, raw_input().split()))
-for i in range(T):
-  a, b, N = l[i][0], l[i][1], l[i][2]
-  val = 0
-  for i in range(0, N): #0, 1, 2, 3, 4
-    val += (pow(2, i) * b)
-    print a + val,
-  print ""
+n = int(raw_input().strip())
+
+for i in range(1, n+1):
+    print " " * (n-i) + "#" * i
